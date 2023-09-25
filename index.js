139e0e327eb0acdafc863859e7e72d14294b7678
Ario Nugraha Ramadhanu <arionugraha5@gmail.com>
2023-09-25 16:34:15 +0700

    add multer function to endpoint

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+require("dotenv").config();
 
-var app = express();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.static("public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+  res.json({ name: originalname, type: mimetype, size });
+});
 
-
-
-const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
